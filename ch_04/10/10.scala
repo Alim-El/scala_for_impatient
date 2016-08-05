71b3e48755a640033071848c00381d8ef3c4a36b
maria <maria@eldis.ru>
2016-08-05 10:52:09 +0700

    ex.10 ch.04 - some code formatting

--- ch_04/10/10.scala
+++ ch_04/10/10.scala
@@ -1,5 +1,6 @@
 object Main extends App {
   println()
-  assert("Hello".zip("World").toArray[(Char, Char)].deep == Array[(Char, Char)](('H','W'), ('e','o'), ('l','r'), ('l','l'), ('o','d')).deep)
+  assert("Hello".zip("World").toArray[(Char, Char)].deep ==
+          Array[(Char, Char)](('H','W'), ('e','o'), ('l','r'), ('l','l'), ('o','d')).deep)
 }
 
